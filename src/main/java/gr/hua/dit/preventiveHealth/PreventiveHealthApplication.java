7bef72c6159d4e36d36b684cb6a5547eb9365239
AlexandrosManolis <it2021132@hua.gr>
2024-11-14 17:53:08 +0200

    Initial commit

---
+++ src/main/java/gr/hua/dit/preventiveHealth/PreventiveHealthApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.preventiveHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventiveHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreventiveHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/hua/dit/preventiveHealth/PreventiveHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.preventiveHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventiveHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
