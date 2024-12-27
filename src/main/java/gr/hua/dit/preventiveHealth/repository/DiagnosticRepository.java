ce9cf198f809f8bf369cab6c8f994a9760a96227
AlexandrosManolis <it2021132@hua.gr>
2024-12-27 11:29:48 +0200

    Add find by specialty

--- src/main/java/gr/hua/dit/preventiveHealth/repository/DiagnosticRepository.java
+++ src/main/java/gr/hua/dit/preventiveHealth/repository/DiagnosticRepository.java
@@ -3,7 +3,10 @@ package gr.hua.dit.preventiveHealth.repository;
 import gr.hua.dit.preventiveHealth.entity.DiagnosticCenter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface DiagnosticRepository extends JpaRepository<DiagnosticCenter, Integer> {
+
+    List<DiagnosticCenter> findBySpecialties(String specialty);
 }
--- src/main/java/gr/hua/dit/preventiveHealth/repository/DoctorRepository.java
+++ src/main/java/gr/hua/dit/preventiveHealth/repository/DoctorRepository.java
@@ -3,7 +3,10 @@ package gr.hua.dit.preventiveHealth.repository;
 import gr.hua.dit.preventiveHealth.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+
+    List<Doctor> findBySpecialty(String specialty);
 }
